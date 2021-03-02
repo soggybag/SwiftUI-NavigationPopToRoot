fc7a692623b65ac40a776945f6825aea0594b709
Mitchell <soggybag@gmail.com>
2021-03-01 16:44:33 -0800

    Initial Commit

---
+++ NavigationPopToRoot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationPopToRoot
+//
+//  Created by Mitchell Hudson on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationPopToRoot/NavigationPopToRootApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPopToRootApp.swift
+//  NavigationPopToRoot
+//
+//  Created by Mitchell Hudson on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPopToRootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
